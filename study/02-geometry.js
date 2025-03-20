a499e0f035060e5fc1ec7076405f11a17d4fde20
chunhyomin <cjsgyals04@naver.com>
2025-03-20 11:18:46 +0900

    02-geometry - heart 실습습

--- study/02-geometry.js
+++ study/02-geometry.js
@@ -35,7 +35,7 @@ class App {
             0.1,
             100    
         );
-        camera.position.z = 2;
+        camera.position.z = 15;
         this._camera = camera;
     }
     _setupLight(){
@@ -45,8 +45,18 @@ class App {
         light.position.set(-1, 2, 4);
         this._scene.add(light);
     }
-    _setupModel(){
-        const geometry = new THREE.TorusKnotGeometry(0.8, 0.2, 100, 16, 2, 3);
+    _setupModel(){        
+        const shape = new THREE.Shape();
+        const x = -2.5, y = -5;
+        shape.moveTo(x + 2.5, y + 2.5);
+        shape.bezierCurveTo(x + 2.5, y + 2.5, x + 2, y, x, y);
+        shape.bezierCurveTo(x - 3, y, x - 3, y + 3.5, x - 3, y + 3.5);
+        shape.bezierCurveTo(x - 3, y + 5.5, x - 1.5, y + 7.7, x + 2.5, y + 9.5);
+        shape.bezierCurveTo(x + 6, y + 7.7, x + 8, y + 4.5, x + 8, y + 3.5);
+        shape.bezierCurveTo(x + 8, y + 3.5, x + 8, y, x + 5, y);
+        shape.bezierCurveTo(x + 3.5, y, x + 2.5, y + 2.5, x + 2.5, y + 2.5);
+        
+        const geometry = new THREE.ShapeGeometry(shape);
         const fillMaterial = new THREE.MeshPhongMaterial({color:0x515151});
         const cube = new THREE.Mesh(geometry, fillMaterial);
         const lineMaterial = new THREE.LineBasicMaterial({color: 0xffff00});
