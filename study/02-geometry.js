9767a7084047397bccfbb563f4e9431b8f24a35b
chunhyomin <cjsgyals04@naver.com>
2025-03-20 16:24:57 +0900

    02-geometry-font 실습습

--- study/02-geometry.js
+++ study/02-geometry.js
@@ -1,101 +1,113 @@
 import * as THREE from '../build/three.module.js';
-import { OrbitControls } from "../examples/jsm/controls/OrbitControls.js"
+import { OrbitControls } from '../examples/jsm/controls/OrbitControls.js';
+import { FontLoader } from '../examples/jsm/loaders/FontLoader.js';
+import { TextGeometry } from '../examples/jsm/geometries/TextGeometry.js';
 
 class App {
     constructor() {
-        const divContainer = document.querySelector("#webgl-container")
-        this._divContainer = divContainer;
+        const divContainer = document.querySelector('#webgl-container');
+        this.divContainer = divContainer;
 
-        const renderer = new THREE.WebGLRenderer({antialias: true});
+        let renderer = new THREE.WebGLRenderer({
+            antialias: true,
+        });
         renderer.setPixelRatio(window.devicePixelRatio);
         divContainer.appendChild(renderer.domElement);
         this._renderer = renderer;
 
         const scene = new THREE.Scene();
         this._scene = scene;
+
         this._setupCamera();
         this._setupLight();
         this._setupModel();
         this._setupControls();
-
+        
         window.onresize = this.resize.bind(this);
         this.resize();
+    
         requestAnimationFrame(this.render.bind(this));
     }
 
-    _setupControls(){
-       new OrbitControls(this._camera, this._divContainer);
-    }
-    _setupCamera(){
-        const width = this._divContainer.clientWidth;
-        const height = this._divContainer.clientHeight;
-        const camera = new THREE.PerspectiveCamera(
-            75,
-            width / height,
-            0.1,
-            100    
-        );
+    _setupCamera() {
+        const width = this.divContainer.clientWidth;
+        const height = this.divContainer.clientHeight;
+        const camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000);
         camera.position.z = 15;
         this._camera = camera;
     }
-    _setupLight(){
+
+    _setupLight() {
         const color = 0xffffff;
         const intensity = 1;
         const light = new THREE.DirectionalLight(color, intensity);
         light.position.set(-1, 2, 4);
         this._scene.add(light);
     }
-    _setupModel(){            
-        const x = -2.5, y = -5;
-        const shape = new THREE.Shape();
-        shape.moveTo(x + 2.5, y + 2.5);
-        shape.bezierCurveTo(x + 2.5, y + 2.5, x + 2, y, x, y);
-        shape.bezierCurveTo(x - 3, y, x - 3, y + 3.5, x - 3, y + 3.5);
-        shape.bezierCurveTo(x - 3, y + 5.5, x - 1.5, y + 7.7, x + 2.5, y + 9.5);
-        shape.bezierCurveTo(x + 6, y + 7.7, x + 8, y + 4.5, x + 8, y + 3.5);
-        shape.bezierCurveTo(x + 8, y + 3.5, x + 8, y, x + 5, y);
-        shape.bezierCurveTo(x + 3.5, y, x + 2.5, y + 2.5, x + 2.5, y + 2.5);
+    
+    _setupControls() {
+        this._controls = new OrbitControls(this._camera, this._renderer.domElement);
+        this._controls.enableDamping = true;
+    }
 
-        const settings = {
-            steps: 1,
-            depth: 4,
-            bevelEnabled: false,
-            bevelThickness: 0.1,
-            bevelSize: 0.1,
-            bevelSegments: 1,
+
+    _setupModel() {
+        const fontLoader = new FontLoader();
+        
+        async function loadFont(that) {
+            const url = "../examples/fonts/helvetiker_regular.typeface.json";
+            const font = await new Promise((resolve, reject) => {
+                fontLoader.load(url, resolve, undefined, reject);
+            });
+            
+            const geometry = new TextGeometry("CHM", {
+                font: font,
+                size: 9,
+                height: 1.8,
+                curveSegments: 4,
+                bevelEnabled: true,
+                bevelThickness: 0.1,
+                bevelSize: 0.1,
+                bevelSegments: 2,
+            });
+            
+            const fillMaterial = new THREE.MeshPhongMaterial({color: 0x515151});
+            const cube = new THREE.Mesh(geometry, fillMaterial);
+            
+            const lineMaterial = new THREE.LineBasicMaterial({color: 0xffff00});
+            const line = new THREE.LineSegments(
+                new THREE.WireframeGeometry(geometry), lineMaterial);
+            
+            const group = new THREE.Group();
+            group.add(cube);
+            group.add(line);
+            
+            that._scene.add(group);
+            that._cube = group;
         };
         
-        const geometry = new THREE.ExtrudeGeometry(shape, settings);
-        const fillMaterial = new THREE.MeshPhongMaterial({color: 0x515151});
-        const cube = new THREE.Mesh(geometry, fillMaterial);
-        const lineMaterial = new THREE.LineBasicMaterial({color: 0xffff00});
-        const line = new THREE.LineSegments(
-            new THREE.WireframeGeometry(geometry), lineMaterial);
-        const group = new THREE.Group()
-        group.add(cube);
-        group.add(line);    
-        this._scene.add(group);
-        this._cube = group;
+        loadFont(this);
     }
-    resize(){
-        const width = this._divContainer.clientWidth;
-        const height = this._divContainer.clientHeight;
+
+    resize() {
+        const width = this.divContainer.clientWidth;
+        const height = this.divContainer.clientHeight;
         this._camera.aspect = width / height;
         this._camera.updateProjectionMatrix();
         this._renderer.setSize(width, height);
     }
-    render(time){
+
+    render(time) {
         this._renderer.render(this._scene, this._camera);
         this.update(time);
         requestAnimationFrame(this.render.bind(this));
     }
-    update(time){
-        time *= 0.001;
-        //this._cube.rotation.x = time;
-        //this._cube.rotation.y = time;
+
+    update(time) {
+        this._controls.update();
     }
 }
 
-window.onload = function(){
-    new App()
+window.onload = function() {
+    new App();
 }
